1cbc123b76ba767dab9d42da425e68e5e89f6335
carmen <davecarmentucker@hotmail.com>
2023-04-04 11:11:58 -0400

    Simple to do list

---
+++ script.js
@@ -0,0 +1,44 @@
+//adding a task
+
+document.querySelector('#push').onclick = function (){
+  if(document.querySelector('#newtask input').value.length == 0){
+    alert("Please Enter A Task To Do")
+  }
+
+    else{
+      document.querySelector('#tasks').innerHTML 
+      += `
+        <div class="task">
+          <span id="taskname">
+        ${document.querySelector('#newtask input').value}
+          </span>
+          <button class="delete">
+          <i class="fa fa-trash" aria-hidden="true"></i>
+            
+          </button>
+        </div>
+      
+      `;
+//Deleting a Task
+      var current_tasks = document.querySelectorAll(".delete");
+      for(var i=0; i<current_tasks.length; i++){
+        current_tasks[i].onclick = function(){
+          this.parentNode.remove();
+        }
+      }
+
+//Cross through completed task
+         var tasks = document.querySelectorAll(".task");
+         for(var i=0; i<tasks.length; i++){
+          tasks[i].onclick = function(){
+            this.classList.toggle('completed');
+          }
+         } 
+
+//Clear to enter new task
+      document.querySelector("#newtask input").
+      value = ""; 
+  
+  
+  }
+}
